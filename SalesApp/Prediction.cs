635dcdf937a2a66b04aaea708f2e68ecd8fd913d
Brock McCall <4952588@student.swin.edu.au>
2018-10-03 14:51:49 +1000

    starting Prediction
    Co-Authored-By: drew2473399 <drew2473399@users.noreply.github.com>

---
+++ SalesApp/Prediction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApp
+{
+    class Prediction
+    {
+
+
+        public void PopulateMonthsArray(int year, int month) {
+
+
+        }
+
+        public void GetLastMonths(int amount)
+        {
+
+            
+        }
+
+
+    }
+}
