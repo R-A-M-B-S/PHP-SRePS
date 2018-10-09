7585962a90294c4cd7b1981a503105eba5e62ebe
mdsimmo <mdsimmo2+git@gmail.com>
2018-10-10 09:15:12 +1100

    Setup test library

---
+++ SalesApp/SalesApp.Tests/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SalesApp.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+			Sale sale = new Sale(1);
+			Assert.AreEqual(sale.saleId, 1);
+        }
+    }
+}
