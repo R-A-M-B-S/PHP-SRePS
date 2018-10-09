1cfea33d78ef3370f6017ff9d4e69940705dad96
mdsimmo <mdsimmo2+git@gmail.com>
2018-10-10 10:26:06 +1100

    Added monthly report tests

---
+++ SalesApp/SalesApp.Tests/MonthlyReportTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System;
+namespace SalesApp.Tests
+{
+    [TestFixture()]
+    public class MonthlyReportTest
+    {
+        [Test()]
+        public void TestMonthlyCountIsCorrect()
+        {
+			// Test data has:
+			//   Asset 1 x14
+            //   Asset 2 x12
+            //   Asset 3 x15
+            //   Asset 4 x3
+			Database db = new Database("test.db").Reset().Connect().Execute(Program.GetTestData());
+            
+			IDictionary<int, int> count = db.CountAssetSales(DateTime.Now.Year, DateTime.Now.Month);
+
+			Assert.AreEqual(14, count[1]);
+			Assert.AreEqual(12, count[2]);
+			Assert.AreEqual(15, count[3]);
+			Assert.AreEqual(3,  count[4]);
+        }
+    }
+}
